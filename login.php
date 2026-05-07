a32b0e69dbd8437370bc3b9eb790987e843407ac
CobraIvi11 <119098882+CobraIvi11@users.noreply.github.com>
2026-05-07 13:21:24 +0200

    schermata login

---
+++ login.php
@@ -0,0 +1,24 @@
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<form id="login-form">
+    <div class="logo-container">
+        <img src="img/Logo.png" alt="Logo" class="brand-logo">
+    </div>
+
+    <h2 class="login-title">Login</h2>
+
+    <label for="email" class="label-text">Email:</label>
+    <input type="email" id="email" class="input-field" name="email" placeholder="Inserisci la tua email" required>
+
+    <label for="password" class="label-text">Password:</label>
+    <input type="password" id="password" class="input-field" name="password" placeholder="Inserisci la password" required>
+
+    <input type="submit" id="login-button" class="btn-submit" value="Accedi">
+
+    <p class="forgot-password">
+        <a href="#" class="forgot-link">Password dimenticata?</a>
+    </p>
+</form>
+</body>
