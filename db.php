fdbe14fba0156ba5421e567a348b6716e5e9a292
CobraIvi11 <119098882+CobraIvi11@users.noreply.github.com>
2026-05-16 11:40:39 +0200

    db corretto

--- db.php
+++ db.php
@@ -1,22 +1,22 @@
 <?php
+
 class Database {
     private static $instance = null;
     private $pdo;
 
     private function __construct() {
-        $host = "localhost";
-        $db_name = "pollaio_iot";
-        $username = "root";
-        $password_db = "";
+        $host     = $_ENV['DB_HOST'] ?? 'localhost';
+        $db_name  = $_ENV['DB_NAME'] ?? 'pollaio_iot';
+        $username = $_ENV['DB_USER'] ?? 'root';
+        $password = $_ENV['DB_PASS'] ?? '';
 
         try {
-            $this->pdo = new PDO("mysql:host=$host;dbname=$db_name;
-            charset=utf8", $username, $password_db);
+            $this->pdo = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $username, $password);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             die("Errore di connessione al database: " . $e->getMessage());
         }
     }
-
 
     public static function getInstance() {
         if (self::$instance === null) {
@@ -24,6 +24,7 @@ class Database {
         }
         return self::$instance;
     }
+
     public function getConnection() {
         return $this->pdo;
     }
